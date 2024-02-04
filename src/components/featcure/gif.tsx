6067e613df0c66d307c2ac577f31c07490101f7b
ErtugrulArac <aracertugrul407@gmail.com>
2024-02-05 01:49:06 +0300

    asdad

--- src/components/featcure/gif.tsx
+++ src/components/featcure/gif.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 const gif = () => {
   return (
-    <div className='w-96 h-96 mt-32 max-md:mt-0 max-md:w-full max-md:h-72 max-md:mb-10 mb-32'>
-        <Image className='w-96 h-96 object-cover max-md:w-full max-md:h-72' src="/site.gif" alt='gif' width={200} height={200} />
+    <div className='w-96 h-96 mt-32 max-md:mt-0 max-md:w-full max-md:h-92 max-md:mb-10 mb-32'>
+        <Image className='w-96 h-96 object-cover max-md:w-full max-md:h-92' src="/site.gif" alt='gif' width={200} height={200} />
     </div>
   )
 }
