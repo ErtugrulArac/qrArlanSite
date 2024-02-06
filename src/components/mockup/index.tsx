2bddd7984da473f9f3a93a5a770c5ff975c63fa4
whcender <heilender31@gmail.com>
2024-02-06 23:03:40 +0300

    fix and 3d

--- src/components/mockup/index.tsx
+++ src/components/mockup/index.tsx
@@ -4,9 +4,9 @@ import Mock from "@/components/mockup/img"
 const index = () => {
     return (
         <div className='bg-white'>
-            <div className='max-md:w-[90%] flex justify-center items-center m-auto max-lg:flex-col  '>
+            <div className='max-md:w-[90%] flex justify-center max-lg:w-[90%] items-center m-auto max-lg:flex-col  '>
                 <div className='flex-1 flex justify-center items-center mt-24 max-md:mt-10 mb-28 max-md:mb-0'>
-                    <img className='size-[550px] object-cover max-lg:size-[100%] ' src="a4yatay.png" alt="" />
+                    <img className='size-[550px] object-cover max-lg:size-[80%] max-md:w-[100%] ' src="a4yatay.png" alt="" />
                 </div>
                 <Mock />
             </div>
