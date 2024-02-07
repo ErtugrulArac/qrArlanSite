4dff5fa20ca61297e460b76565f6cc77f1d59879
ErtugrulArac <aracertugrul407@gmail.com>
2024-02-07 21:28:29 +0300

    sorunmockup

--- src/components/mockup/index.tsx
+++ src/components/mockup/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import Mock from "@/components/mockup/img"
+
 
 const index = () => {
     return (
-        <div className='bg-white'>
-            <div className='max-md:w-[90%] flex justify-center max-lg:w-[90%] items-center m-auto max-lg:flex-col  '>
-                <div className='flex-1 flex justify-center items-center mt-24 max-md:mt-10 mb-28 max-md:mb-0'>
-                    <img className='size-[550px] object-cover max-lg:size-[80%] max-xl:size-[60%] max-md:w-[100%] ' src="a4yatay.png" alt="" />
-                </div>
-                <Mock />
-            </div>
+        <div className='bg-white '>
+          asdasdasd
         </div>
     )
 }
