24a0849bd3d855372d328ed06e9647284bffb1e9
ErtugrulArac <aracertugrul407@gmail.com>
2024-02-07 22:11:19 +0300

    mockupfixed

--- src/components/mockup/index.tsx
+++ src/components/mockup/index.tsx
@@ -4,9 +4,9 @@ import Right from './right'
 
 const index = () => {
     return (
-        <div className='bg-white text-black py-8'>
+        <div className='bg-white text-black max-md:py-8'>
           <div className='xl:w-[66%] flex justify-between items-center max-lg:flex-col max-lg:w-[90%] max-xl:w-[90%]  m-auto'>
-            <img className='w-[40%] max-md:w-[90%]' src="a4yatay.png" alt="menu" />
+            <img className='w-[40%] max-md:w-[100%]' src="a4yatay.png" alt="menu" />
             <Right />
           </div>
         </div>
