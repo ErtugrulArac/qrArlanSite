de066724962edfbb2952cf53d0cd00af288b1d04
whcender <heilender31@gmail.com>
2024-02-07 22:20:22 +0300

    sorular responsive fix

--- src/components/sorular/index.tsx
+++ src/components/sorular/index.tsx
@@ -6,8 +6,8 @@ import Faq from "@/components/sorular/faq";
 export default function Home() {
   return (
     <div className="bg-white py-12">
-      <div className="flex w-[90%] max-xl:w-[95%] m-auto justify-center items-center gap-12 max-lg:flex-col ">
-        <div className="flex justify-center items-center flex-col">
+      <div className="flex w-[90%] max-xl:w-[90%] m-auto justify-center items-center gap-12 max-lg:flex-col ">
+        <div className="flex justify-center items-center flex-col max-xl:flex-1">
           <img className="w-[30%] max-md:w-[60%]" src="soruisareti.png" alt="" />
           <p className="text-4xl max-md:text-3xl max-lg:w-[90%] font-fontum font-black w-[40%] text-center tracking-wider max-md:w-full">Sıkça Sorulan Sorular (SSS)</p>
         </div >
