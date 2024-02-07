0145de3ebf7bf3b93fcc02e8b273b43842faa8df
ErtugrulArac <aracertugrul407@gmail.com>
2024-02-07 20:02:28 +0300

    asda

--- src/components/sorular/index.tsx
+++ src/components/sorular/index.tsx
@@ -6,7 +6,7 @@ import Faq from "@/components/sorular/faq";
 export default function Home() {
   return (
     <div className="bg-white py-12">
-      <div className="flex m-auto justify-center items-center gap-5 max-lg:flex-col ">
+      <div className="flex m-auto justify-center items-center gap-12 max-lg:flex-col ">
         <div className="flex justify-center items-center flex-col">
           <img className="w-[30%] max-md:w-[60%]" src="soruisareti.png" alt="" />
           <p className="text-4xl max-md:text-3xl max-lg:w-[90%] font-fontum font-black w-[40%] text-center tracking-wider max-md:w-full">Sıkça Sorulan Sorular (SSS)</p>
