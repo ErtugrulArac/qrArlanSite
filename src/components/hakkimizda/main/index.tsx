0628aacde87282f96fb880f493e91f142b1f2d08
ErtugrulArac <aracertugrul407@gmail.com>
2024-02-13 16:40:35 +0300

    asd

--- src/components/hakkimizda/main/index.tsx
+++ src/components/hakkimizda/main/index.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 const index = () => {
     return (
         <div className='mt-44 max-md:py-12  max-md:mt-0 py-14  text-black bg-white'>
-    <div className='flex w-[60%] max-md:w-full m-auto items-center justify-around max-md:flex-col max-md:gap-9'>
+    <div className='flex w-[60%] max-md:w-full m-auto items-center justify-around max-lg:flex-col max-md:gap-9 max-lg:gap-10'>
                 <div className='flex flex-col gap-7 max-md:w-[95%] max-md:mt-10 '>
-                    <h3 className='text-8xl max-md:text-4xl font-fontpopins font-bold '>Hakkımızda</h3>
-                    <p className='text-2xl max-md:text-lg leading-relaxed font-medium w-[90%] max-md:opacity-80'>We're a diverse bunch of individuals with a passion for creating brands and websites we can all be proud of.</p>
+                    <h3 className='text-6xl max-md:text-4xl font-fontpopins font-bold max-xl:text-5xl'>Hakkımızda</h3>
+                    <p className='text-2xl max-md:text-base leading-relaxed font-medium w-[90%] max-md:opacity-70'>We're a diverse bunch of individuals with a passion for creating brands and websites we can all be proud of.</p>
                 </div>
-                <img className='w-[500px] rounded-3xl max-md:rounded-none  max-md:w-full' src="arlan.gif" alt="" />
+                <img className='w-[500px] rounded-3xl max-md:rounded-none  max-md:w-full max-xl:ml-10 max-lg:ml-0' src="arlan.gif" alt="" />
             </div>
         </div>
     )
--- src/components/hakkimizda/vizyon/index.tsx
+++ src/components/hakkimizda/vizyon/index.tsx
@@ -5,11 +5,11 @@ const index = () => {
     <div className='bg-white '>
       <div className='w-[60%] max-md:w-[95%] m-auto'>
         <h3 className='text-3xl font-fontpopins font-bold'>Vizyonumuz</h3>
-        <div className='flex max-md:flex-col gap-36 max-md:gap-12 mt-10 max-md:mt-7'>
-          <p className='font-semibold  max-md:opacity-80'>
+        <div className='flex max-lg:flex-col gap-36 max-md:gap-12 max-lg:gap-12 mt-10 max-md:mt-7'>
+          <p className='font-semibold max-md:text-sm  max-md:opacity-70'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, officia mollitia! Cum porro exercitationem doloribus harum? Numquam minus, pariatur beatae reprehenderit vitae voluptatum mollitia aliquid hic eum, sed dolorum! Tempora.
           </p>
-          <p className='font-semibold max-md:opacity-80 mb-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque temporibus voluptatibus cupiditate vitae repudiandae, excepturi accusantium. Delectus eius molestiae asperiores illum id! Quo praesentium vel quos dicta totam aut illum?</p>
+          <p className='font-semibold max-md:text-sm max-md:opacity-70 mb-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque temporibus voluptatibus cupiditate vitae repudiandae, excepturi accusantium. Delectus eius molestiae asperiores illum id! Quo praesentium vel quos dicta totam aut illum?</p>
         </div>
       </div>
     </div>
