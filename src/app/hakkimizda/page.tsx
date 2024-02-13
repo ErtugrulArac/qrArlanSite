be1334eb15874f17be88e0aa6c039865912e2da4
ErtugrulArac <aracertugrul407@gmail.com>
2024-02-13 16:03:56 +0300

    çizgi hakkmızda

--- src/app/hakkimizda/page.tsx
+++ src/app/hakkimizda/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Main from "@/components/hakkimizda/main/"
 import Mockup from "@/components/hakkimizda/mockup/index"
+import Vizyon from "@/components/hakkimizda/vizyon/index"
 
 const page = () => {
     return (
         <div >
             <Main />
             <Mockup />
+            <Vizyon />
         </div>
     )
 }
---
+++ src/components/hakkimizda/vizyon/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='bg-white '>
+      <div className='w-[60%] max-md:w-[95%] m-auto'>
+        <h3 className='text-3xl font-fontpopins font-bold'>Vizyonumuz</h3>
+        <div className='flex max-md:flex-col gap-36 max-md:gap-12 mt-10 max-md:mt-7'>
+          <p className='font-semibold'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, officia mollitia! Cum porro exercitationem doloribus harum? Numquam minus, pariatur beatae reprehenderit vitae voluptatum mollitia aliquid hic eum, sed dolorum! Tempora.
+          </p>
+          <p className='font-semibold mb-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque temporibus voluptatibus cupiditate vitae repudiandae, excepturi accusantium. Delectus eius molestiae asperiores illum id! Quo praesentium vel quos dicta totam aut illum?</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default index
