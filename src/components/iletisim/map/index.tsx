826a7488c45f39423fe6ecc196ef53d3a64a4eb9
ErtugrulArac <aracertugrul407@gmail.com>
2024-02-16 16:27:50 +0300

    map

--- src/components/iletisim/map/index.tsx
+++ src/components/iletisim/map/index.tsx
@@ -23,7 +23,7 @@ const index = () => {
                 <Form />
             </div>
             <div className='h-full flex-1 max-md:h-8 '>
-                <iframe className='max-md:h-80 ' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12242.223970457842!2d41.2707265!3d39.9065722!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2628a79910cc9b5f%3A0xd45613ce0f4c8482!2sArlan%20Medya%20-%20Dijital%20Reklam%20Ajans%C4%B1!5e0!3m2!1str!2str!4v1707910390807!5m2!1str!2str" width="100%" height="580" style={{ backgroundColor: "black" }} loading="lazy" ></iframe>
+                <iframe className='max-md:h-96 ' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12242.223970457842!2d41.2707265!3d39.9065722!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2628a79910cc9b5f%3A0xd45613ce0f4c8482!2sArlan%20Medya%20-%20Dijital%20Reklam%20Ajans%C4%B1!5e0!3m2!1str!2str!4v1707910390807!5m2!1str!2str" width="100%" height="580" style={{ backgroundColor: "black" }} loading="lazy" ></iframe>
             </div>
 
         </div>
