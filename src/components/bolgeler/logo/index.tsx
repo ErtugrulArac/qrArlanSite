92e629bc098917281cd1b3ff5ae03c8e0adc8d91
ErtugrulArac <aracertugrul407@gmail.com>
2024-02-28 18:39:31 +0300

    referanslar güncellendi

--- src/components/bolgeler/logo/index.tsx
+++ src/components/bolgeler/logo/index.tsx
@@ -11,7 +11,7 @@ import {
 
 export default function CarouselSpacing() {
   return (
-    <div className="justify-center items-center w-full py-20 max-lg:py-10 bg-white flex">
+    <div className="justify-center items-center w-full py-20 max-lg:pb-10 max-lg:pt-2 max-lg:py-0  bg-white flex">
 
       <Carousel
        plugins={[
@@ -22,7 +22,7 @@ export default function CarouselSpacing() {
       className="w-full max-w-[90%] max-sm:w-[90%] max-lg:w-[80%]">
         <CarouselContent className="-ml-1">
           {logolar.map((item) => (
-            <CarouselItem key={item.id} className="pl-1 basis-[40%] md:basis-[30%] lg:basis-[15%]">
+            <CarouselItem key={item.id} className="pl-1 basis-[60%] md:basis-[30%] lg:basis-[15%]">
               <div className="p-1">
                 <img src={item.logo} alt={item.name} />
               </div>
--- src/components/referanslar/index.tsx
+++ src/components/referanslar/index.tsx
@@ -4,7 +4,7 @@ import Right from './referans'
 
 const index = () => {
     return (
-        <div className='bg-white text-black max-md:py-20 py-16'>
+        <div className='bg-white text-black max-md:pt-12 md:py-16 max-md:pb-3 '>
           <div className='xl:w-[66%] flex justify-between items-center max-lg:flex-col max-lg:w-[90%] max-xl:w-[90%]  m-auto max-lg:gap-14'>
             <img className='w-[40%] max-md:w-[100%]' src="referanslar.webp" alt="qr menu referanslar" />
             <Right />
