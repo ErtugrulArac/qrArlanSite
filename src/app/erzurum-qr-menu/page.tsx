c9a3a85d27070f4be2ee37609918150a2f5e460e
ErtugrulArac <aracertugrul407@gmail.com>
2024-02-28 22:52:48 +0300

    sikimsitesini

--- src/app/erzurum-qr-menu/page.tsx
+++ src/app/erzurum-qr-menu/page.tsx
@@ -10,29 +10,29 @@ const page = () => {
             <Head>
                 <title>Arlan Medya - Erzurum Qr Menu</title>
             </Head>
-            <Ust header="ArlanMedya Erzurum" text="ArlanMedya, Erzurum'da faaliyet gösteren restoranlar için özel olarak tasarlanmış QR menü çözümleri sunmaktadır. Bu yenilikçi sistem, misafirlerin restoran menülerine kolay ve hızlı erişimini sağlar, aynı zamanda restoran sahiplerine kağıt israfını azaltma ve hijyen standartlarını koruma imkanı sunar. Erzurum'daki restoranlar için QR menü sistemleri, modern teknolojiyi kullanarak daha verimli ve çevre dostu bir yemek deneyimi sunmaktadır." />
+            <Ust header="ArlanMedya Erzurum" text="ArlanMedya, Erzurum'da faaliyet gösteren restoranlar için özel olarak tasarlanmış QR menü çözümleri sunmaktadır. Bu yenilikçi sistem, misafirlerin restoran menülerine kolay ve hızlı erişimini sağlar, aynı zamanda restoran sahiplerine kağıt israfını azaltma ve hijyen standartlarını koruma imkanı sunar." />
             <Logo />
-            <Telefon header="Erzurum Qr Menü Sistemleri" text="Erzurum'daki restoranlar için QR menü kullanmanın birçok avantajı bulunmaktadır. İşte bunlardan bazıları:" list={["Veri Analitiği: QR menüleri, restoran sahiplerine müşteri tercihleri ve sipariş alışkanlıkları hakkında değerli veri sağlar. Bu veriler, menüyü optimize etmek ve müşteri memnuniyetini artırmak için kullanılabilir.", "Yer Tasarrufu: QR menüleri, restoran içinde yer kaplayan geleneksel menülerin yerine dijital olarak sunulduğu için işletmeler için fiziksel alanda tasarruf sağlar. Bu da daha fazla masa veya oturma alanı oluşturma imkanı sunar.", "Kapsamlı Bilgi: QR menüleri, her yemek veya içecek öğesi için detaylı açıklama ve görseller sunma imkanı sağlar. Müşteriler, sipariş vermeden önce ürünler hakkında daha fazla bilgi edinebilir ve daha bilinçli seçimler yapabilir.", "Zaman: QR menüleri sayesinde müşteriler, geleneksel menülerde zaman harcamak yerine akıllı telefonlarını kullanarak hızlıca yemek seçeneklerine erişebilirler."]} />
+            <Telefon header="Erzurum Qr Menü Sistemleri " text="Erzurum'daki restoranlar için QR menü kullanmanın birçok avantajı bulunmaktadır. İşte bunlardan bazıları:" list={[" QR menüleri, restoran sahiplerine müşteri tercihleri ve sipariş alışkanlıkları hakkında değerli veri sağlar.", " QR menüleri, restoran içinde yer kaplayan geleneksel menülerin yerine dijital olarak sunulduğu için işletmeler için fiziksel alanda tasarruf sağlar. ", " QR menüleri, her yemek veya içecek öğesi için detaylı açıklama ve görseller sunma imkanı sağlar.", " QR menüleri sayesinde müşteriler, geleneksel menülerde zaman harcamak yerine akıllı telefonlarını kullanarak hızlıca yemek seçeneklerine erişebilirler."]} />
             {/* list 4 TANE OLACAK! */}
             <div className='text-white flex flex-col gap-6 w-[80%] max-md:w-[90%] m-auto mt-10'>
-                <div className='flex flex-col gap-5 '>
-                    <h3 className='text-4xl max-md:text-3xl'>
-                        Erzurum Qr Menü Fiyatları
+                <div className='flex flex-col gap-5  '>
+                    <h3 className='text-4xl w-[80%] font-semibold max-md:text-3xl'>
+                        Erzurum qr menü fiyatları?
                     </h3>
                     <p className='w-[70%] max-md:w-full font-semibold opacity-80 text-sm'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur commodi est at et eligendi error pariatur aliquid totam beatae, enim quidem nobis neque sit voluptatem accusantium recusandae dicta molestiae iusto!
+                        Erzurum'da QR menü fiyatları, genellikle işletmenin ihtiyaçlarına, menünün özelliklerine ve hizmet sağlayıcısının sunduğu paketlere göre değişiklik gösterebilir. Fiyatlandırma hakkında daha detaylı bilgi almak için lütfen bize özel mesaj yoluyla ulaşın. Arlan Medya olarak Erzurum'daki işletmeler için uygun fiyatlı ve özelleştirilebilir QR menü çözümleri sunmaktan memnuniyet duyarız. Bizimle iletişime geçmek için anahtar kelimeleri içeren özel bir mesaj atabilirsiniz.
                     </p>
                 </div>
                 <div className='flex flex-col gap-5 '>
-                    <h3 className='text-4xl max-md:text-3xl '>
-                        Erzurum Qr Menü Fiyatları
+                    <h3 className='text-4xl w-[80%] max-md:w-full font-semibold max-md:text-3xl '>
+                        Erzurum'daki restoranlar için QR menü kullanımı, müşteri memnuniyetini nasıl artırabilir?
                     </h3>
                     <p className='w-[70%] max-md:w-full font-semibold opacity-80 text-sm'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur commodi est at et eligendi error pariatur aliquid totam beatae, enim quidem nobis neque sit voluptatem accusantium recusandae dicta molestiae iusto!
+                    Erzurum'daki restoranlar için QR menü kullanımı, işletmelerin ve müşterilerin deneyimini modernize edebilir ve geleneksel kağıt menülerin yerini alabilir. QR menüler, müşterilere fiziksel menülere dokunmadan kolayca menüye erişme ve sipariş vermeleri imkanı sağlar. Bu da işletmeler için daha hijyenik bir çözüm sunar ve müşterilerin sağlık ve güvenlik endişelerini azaltır. Ayrıca, QR menülerin dijital formatı, işletmelerin menüyü kolayca güncellemesine olanak tanır ve kağıt israfını azaltır. Bu özellikler, restoran işletmecilerine operasyonel esneklik sağlayarak işletmelerinin verimliliğini artırabilir. Bununla birlikte, QR menü kullanımı için gerekli olan akıllı telefon veya internet erişimi olmayan bazı müşteriler için erişilebilirlik bir zorluk olabilir. Bu durumda, işletmelerin kağıt menü seçeneğini koruması veya alternatif çözümler sunması gerekebilir bunlara kolaylıkla ArlanMedya tarafından sahip olabilirler.
                     </p>
                 </div>
                 <div className='flex flex-col gap-5 '>
-                    <h3 className='text-4xl max-md:text-3xl'>
+                    <h3 className='text-4xl font-semibold max-md:text-3xl'>
                         Erzurum Qr Menü Fiyatları
                     </h3>
                     <p className='w-[70%] max-md:w-full font-semibold opacity-80 text-sm'>
@@ -40,7 +40,7 @@ const page = () => {
                     </p>
                 </div >
                 <div className='flex flex-col gap-5 '>
-                    <h4 className='text-4xl max-md:text-3xl'>
+                    <h4 className='text-4xl font-semibold max-md:text-3xl'>
                         Erzurum Qr Menü Fiyatları
                     </h4>
                     <p className='w-[70%] max-md:w-full font-semibold opacity-80 text-sm'>
@@ -48,7 +48,7 @@ const page = () => {
                     </p>
                 </div>
                 <div className='flex flex-col gap-5 '>
-                    <h4 className='text-4xl max-md:text-3xl'>
+                    <h4 className='text-4xl font-semibold max-md:text-3xl'>
                         Erzurum Qr Menü Fiyatları
                     </h4 >
                     <p className='w-[70%] max-md:w-full font-semibold opacity-80 text-sm'>
@@ -56,7 +56,7 @@ const page = () => {
                     </p>
                 </div>
                 <div className='flex flex-col gap-5 '>
-                    <h4 className='text-4xl max-md:text-3xl'>
+                    <h4 className='text-4xl font-semibold max-md:text-3xl'>
                         Erzurum Qr Menü Fiyatları
                     </h4>
                     <p className='w-[70%] max-md:w-full font-semibold opacity-80 text-sm'>
@@ -64,7 +64,7 @@ const page = () => {
                     </p>
                 </div>
                 <div className='flex flex-col gap-5 '>
-                    <h4 className='text-4xl max-md:text-3xl'>
+                    <h4 className='text-4xl font-semibold max-md:text-3xl'>
                         Erzurum Qr Menü Fiyatları
                     </h4>
                     <p className='w-[70%] max-md:w-full font-semibold opacity-80 text-sm'>
--- src/components/bolgeler/telefon/index.tsx
+++ src/components/bolgeler/telefon/index.tsx
@@ -14,10 +14,10 @@ const index = ({header, text, list}: Props) => {
                     <h1 className='text-6xl max-lg:text-4xl  leading-[70px] w-full'>{header}</h1>
                     <p className='text-base max-lg:text-sm leading-[22px] opacity-80'>{text}</p>
                     <ul className='flex flex-col gap-4'>
-                        <li className='flex gap-4 opacity-80 font-bold'><img src="koksal.svg" alt="" />{list[0]}</li>
-                        <li className='flex gap-4 opacity-80 font-bold'><img src="koksal.svg" alt="" />{list[1]}</li>
-                        <li className='flex gap-4 opacity-80 font-bold'><img src="koksal.svg" alt="" />{list[2]}</li>
-                        <li className='flex gap-4 opacity-80 font-bold'><img src="koksal.svg" alt="" />{list[3]}</li>
+                        <li className='flex max-md:text-sm gap-4 opacity-80 font-bold'><img src="koksal.svg" alt="" />{list[0]}</li>
+                        <li className='flex max-md:text-sm gap-4 opacity-80 font-bold'><img src="koksal.svg" alt="" />{list[1]}</li>
+                        <li className='flex max-md:text-sm gap-4 opacity-80 font-bold'><img src="koksal.svg" alt="" />{list[2]}</li>
+                        <li className='flex max-md:text-sm gap-4 opacity-80 font-bold'><img src="koksal.svg" alt="" />{list[3]}</li>
                     </ul>
                     <a title='qr menü info' className='w-max px-7 py-2 rounded-xl font-semibold bg-[#8b38cb] text-white whitespace-nowrap xl:text-lg mt-3' href="/hakkimizda"> şimdi bilgi alın</a>
                 </div>
